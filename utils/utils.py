3994ef0965a5675f086b1c6f580b27a74088243d
melen33 <st251100@163.com>
2026-05-29 15:04:04 +0800

    Add files via upload

---
+++ utils/utils.py
@@ -0,0 +1,91 @@
+import json
+import torch
+# import pandas as pd
+from pathlib import Path
+from itertools import repeat
+from collections import OrderedDict
+import errno
+import os
+import os.path as op
+import yaml
+import random
+import numpy as np
+from omegaconf import OmegaConf
+
+def mkdir(path):
+    # if it is the current folder, skip.
+    if path == '':
+        return
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+def load_config_file(file_path):
+    with open(file_path, 'r') as fp:
+        return OmegaConf.load(fp)
+
+def set_seed(seed=1000):
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+
+def load_from_yaml_file(yaml_file):
+    with open(yaml_file, 'r') as fp:
+        return yaml.load(fp, Loader=yaml.FullLoader)
+
+
+def find_file_path_in_yaml(fname, root):
+    if fname is not None:
+        if op.isfile(fname):
+            return fname
+        elif op.isfile(op.join(root, fname)):
+            return op.join(root, fname)
+        else:
+            raise FileNotFoundError(
+                errno.ENOENT, os.strerror(errno.ENOENT), op.join(root, fname)
+            )
+
+
+def ensure_dir(dirname):
+    dirname = Path(dirname)
+    if not dirname.is_dir():
+        dirname.mkdir(parents=True, exist_ok=False)
+
+def read_json(fname):
+    fname = Path(fname)
+    with fname.open('rt') as handle:
+        return json.load(handle, object_hook=OrderedDict)
+
+def write_json(content, fname):
+    fname = Path(fname)
+    with fname.open('wt') as handle:
+        json.dump(content, handle, indent=4, sort_keys=False)
+
+def inverse_sigmoid(x, eps=1e-3):
+    x = x.clamp(min=0, max=1)
+    x1 = x.clamp(min=eps)
+    x2 = (1 - x).clamp(min=eps)
+    return torch.log(x1/x2)
+
+class GradMulConst(torch.autograd.Function):
+    """
+    This layer is used to create an adversarial loss.
+    """
+    @staticmethod
+    def forward(ctx, x, const):
+        ctx.const = const
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output * ctx.const, None
+
+def grad_mul_const(x, const):
+    return GradMulConst.apply(x, const)
